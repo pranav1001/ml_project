e7a7673ad293cc150685b9ea34ed88ddb46e2f3d
Pranav Nayak <34138578+pranav1001@users.noreply.github.com>
2019-11-28 15:04:42 +0530

    Update graph (1).py

--- graph (1).py
+++ graph (1).py
@@ -1,6 +1,6 @@
 import matplotlib
 import pandas as pd
-
+# Plots the accuracy on a graph.
 result=pd.read_csv(r"C:\Users\ashmit\Desktop\accuracies.csv",sep=',',encoding="ISO-8859-1")
 accuracy=result.iloc[1:7,1].values
 time=result.iloc[1:7,2].values
